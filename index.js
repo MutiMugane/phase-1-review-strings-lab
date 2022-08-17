a117a145e8b14a829fbcacc029ac748a92b02789
+ <+>
2022-08-17 09:25:43 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+const currentUser = 'Yes Bana';
+
+//const welcomeMessage = 'Karibu Flatbook, ';
+//const welcomeMessage = currentUser;
+
+//const welcomeMessage = 'Welcome to Flatbook, currentUser!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = 'Welcome, ';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
